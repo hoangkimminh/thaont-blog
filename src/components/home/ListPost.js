d0aae72f3dfbb2c719026081a0839c87d166df42
hoangkimminh <vatliyeutoi@gmail.com>
2020-03-10 18:25:47 +0700

    Fix list post ordering

--- src/components/home/ListPost.js
+++ src/components/home/ListPost.js
@@ -24,7 +24,7 @@ const ListPost = () => {
             createAt: childData.createAt
           })
         })
-        setListPost(data)
+        setListPost(data.reverse())
         setIsLoading(false)
       })
   }, [])
